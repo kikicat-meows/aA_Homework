9f4c88a6f7d886bcb6f6adc6e1b47a57128b30e0
Helen Cheung <helenkei.cheung@gmail.com>
2019-11-01 02:25:11 -0700

    adding W3D5 ADT / DFS homework

---
+++ W3D5/homework.rb
@@ -0,0 +1,102 @@
+p '----- Exercise 1 - Stack'
+
+# Let's write a Stack class. To do this, use the following framework:
+
+  class Stack
+    def initialize
+      # create ivar to store stack here!
+      @arr = []
+    end
+
+    def push(el)
+      # adds an element to the stack
+      arr.push(el)
+      el
+    end
+
+    def pop
+      # removes one element from the stack
+      arr.pop
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+      arr.last
+    end
+
+    attr_reader :arr
+  end
+
+# To test that your code works, create a new instance of the Stack class, and play around with adding and removing elements. Remember, a stack follows the principle of LIFO!
+
+
+p '----- Exercise 2 - Queue'
+
+# Now let's write a Queue class. We will need the following instance methods: enqueue(el), dequeue, and peek.
+
+class Queue
+    def initialize
+        @arr = []        
+    end
+
+    def enqueue(el)
+        arr.push(el)
+        el
+    end
+
+    def dequeue
+        arr.shift
+    end
+
+    def peek
+        arr.first
+    end
+
+    attr_reader :arr
+end
+
+# Test your code to ensure it follows the principle of FIFO.
+
+
+p '----- Exercise 3 - Map'
+
+# As you saw in the videos for tonight, a map can be implemented using a 2D array. Let's write a Map class (following a similar pattern to Stack and Queue) that implements a map using only arrays.
+
+# Remember, a map stores information in key-value pairs, where the keys are always unique. When implemented with arrays, a map might look something like this: my_map = [[k1, v1], [k2, v2], [k3, v2], ...].
+
+# Our Map class should have the following instance methods: set(key, value), get(key), delete(key), show. Note that the set method can be used to either create a new key-value pair or update the value for a pre-existing key. It's up to you to check whether a key currently exists!
+
+# As always, test your code thoroughly to make sure all properties of maps are enforced.
+
+class Map
+    def initialize
+        @arr = []
+    end
+
+    def set(key, value)
+        key_idx = arr.index { |pair| pair[0] == key }
+        if key_idx #if it exists
+            arr[key_idx][1] == value
+        else
+            arr << [key, value]
+        end
+    end
+
+    def get(key)
+        arr.each { |pair| return pair[1] if pair[0] == key}
+    end
+
+    def delete(key)
+        arr.reject! { |pair| pair[0] == key }
+    end
+
+    def deep_dup(arr)
+        arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+    end
+
+    def show
+        deep_dup(arr)
+    end
+    
+    attr_reader :arr
+end
